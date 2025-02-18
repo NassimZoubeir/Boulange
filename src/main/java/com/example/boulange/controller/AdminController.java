587ed3ef97b3e4d53784d2b12f70f83d17dd02a2
Nassim <nassimz@outlook.fr>
2025-02-18 15:14:06 +0100

    Ajout des marques dans la partie admin

--- src/main/java/com/example/boulange/controller/AdminController.java
+++ src/main/java/com/example/boulange/controller/AdminController.java
@@ -8,7 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.boulange.entity.Marque;
+import com.example.boulange.repository.MarqueRepository;
 import com.example.boulange.repository.UtilisateurRepository;
 
 
@@ -16,17 +19,32 @@ import com.example.boulange.repository.UtilisateurRepository;
 public class AdminController {
 	
 	 @Autowired
-	    private UtilisateurRepository utilisateurRepository;
+	 private UtilisateurRepository utilisateurRepository;
+	 
+	 @Autowired
+	 private MarqueRepository marqueRepository;
 
 	    @GetMapping("/admin")
 	    public String adminTableau(Model model) {
 	        model.addAttribute("utilisateurs", utilisateurRepository.findAll());
+	        model.addAttribute("marques", marqueRepository.findAll());
 	        return "admin";
 	    }
 
 	    @PostMapping("/admin/supprimerUtilisateur/{id}")
 	    public String supprimerUtilisateur(@PathVariable Long id) {
 	        utilisateurRepository.deleteById(id);
+	        return "redirect:/admin";
+	    }
+	    @PostMapping("/admin/creerMarque")
+	    public String creerMarque(@RequestParam String nom) {
+	        Marque marque = new Marque(nom);
+	        marqueRepository.save(marque);
+	        return "redirect:/admin";
+	    }
+	    @PostMapping("/admin/supprimerMarque/{id}")
+	    public String supprimerMarque(@PathVariable Long id) {
+	        marqueRepository.deleteById(id);
 	        return "redirect:/admin";
 	    }
 }
